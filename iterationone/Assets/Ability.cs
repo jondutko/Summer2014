dfe0c55b9a60c8a5fd941e7b6eb1dd026cb6e439
jondutko <jonathan.dutko@tufts.edu>
2014-06-10 20:00:02 -0400

    Ability/Job refinement part 1
    Beachhead in definition of abilities and jobs

---
+++ iterationone/Assets/Ability.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ability : MonoBehaviour {
+
+	public string abilName, abilDescription;
+
+}
--- iterationone/Assets/Job.cs
+++ iterationone/Assets/Job.cs
@@ -1,27 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Job : MonoBehaviour {
 
 	public string title;
 	public Job prereqJob;
 	public int prereqLevel;
-	public Quest prereqQuest;
+	public List<Ability> jobAbilities = new List<Ability>();
+	//public Quest prereqQuest;
 
 	public void initializeJob (string newTitle){
 			title = newTitle;
 			prereqJob = null;
 			prereqLevel = 0;
-			prereqQuest = null;
+			//prereqQuest = null;
 	}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
